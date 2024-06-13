c3856dfe32c4df1f3e35867a524243a4d30eb2b1
Nathan <nathan.dsouza316@gmail.com>
2024-06-13 14:01:39 +1200

    removed project grid from homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import PagesMetaHead from '../components/PagesMetaHead'
-import ProjectsGrid from '../components/projects/ProjectsGrid'
 import Button from '../components/reusable/Button'
 import AppBanner from '../components/shared/AppBanner'
 
@@ -10,8 +9,6 @@ export default function Home() {
             <PagesMetaHead title="Home" />
 
             <AppBanner />
-
-            <ProjectsGrid />
 
             <div className="mt-10 sm:mt-15 flex justify-center">
                 <div className="font-general-medium flex items-center px-6 py-3 rounded-lg shadow-lg hover:shadow-xl bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 text-white text-lg sm:text-xl duration-300">
